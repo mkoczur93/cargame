b1beccf31d735259a86b3cfcd492d2b7f8028243
mkoczur93 <48414046+mkoczur93@users.noreply.github.com>
2021-02-03 11:05:58 +0100

    Create MainMenu

---
+++ Assets/Scripts/MenuViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityWeld.Binding;
+using System.ComponentModel;
+
+[Binding]
+public class MenuViewModel : MonoBehaviour, INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    [SerializeField]
+    private Canvas mainMenu = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu = mainMenu.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainMenu.enabled = !mainMenu.enabled;
+            Cursor.visible = mainMenu.enabled;
+        }
+    }
+    [Binding]
+    public void buttonResume()
+    {
+        mainMenu.enabled = !mainMenu.enabled;
+        Cursor.visible = mainMenu.enabled;
+
+    }
+}
+
